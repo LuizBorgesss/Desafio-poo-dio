99e1176ef5c36291e3f227167191df5641ab8244
LuizBorgesss <luizeduardobrgs@gmail.com>
2023-02-23 16:11:15 -0300

    Mentoria e curso

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import br.com.dio.dominio.Curso;
+import br.com.dio.dominio.Mentoria;
+
+import java.time.LocalDate;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Descrição curso Java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso Javascript");
+        curso2.setDescricao("Descrição curso Javascript");
+        curso2.setCargaHoraria(7);
+
+        Mentoria mentoria1 = new Mentoria();
+        mentoria1.setTitulo("Mentoria de Java");
+        mentoria1.setDescricao("Descrição mentoria Java");
+        mentoria1.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria1);
     }
 }
